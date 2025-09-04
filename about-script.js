cb92655288f1463b346b43b83f7c328bec22c8eb
Ebai99 <abibaron99@gmail.com>
2025-09-04 14:46:12 +0100

    disabled all javascript animations

--- about-script.js
+++ about-script.js
@@ -56,30 +56,30 @@ document.addEventListener("DOMContentLoaded", () => {
   showNextDiv();
 
   // Hero section animation on page load
-  window.onload = () => {
-    const heroContent = document.querySelector(".hero-content");
-    heroContent.classList.add("visible");
-  };
+  // window.onload = () => {
+  //   const heroContent = document.querySelector(".hero-content");
+  //   heroContent.classList.add("visible");
+  // };
 
-  // Scroll-based animations for other sections
-  const slideElements = document.querySelectorAll(
-    ".slide-in-left, .slide-in-right, .slide-in-bottom, .slide-in-top"
-  );
-  const observer = new IntersectionObserver(
-    (entries, observer) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add("visible");
-          observer.unobserve(entry.target); // Stop observing after animation
-        }
-      });
-    },
-    {
-      threshold: 0.1, // Trigger when 10% of element is visible
-    }
-  );
+  // // Scroll-based animations for other sections
+  // const slideElements = document.querySelectorAll(
+  //   ".slide-in-left, .slide-in-right, .slide-in-bottom, .slide-in-top"
+  // );
+  // const observer = new IntersectionObserver(
+  //   (entries, observer) => {
+  //     entries.forEach((entry) => {
+  //       if (entry.isIntersecting) {
+  //         entry.target.classList.add("visible");
+  //         observer.unobserve(entry.target); // Stop observing after animation
+  //       }
+  //     });
+  //   },
+  //   {
+  //     threshold: 0.1, // Trigger when 10% of element is visible
+  //   }
+  // );
 
-  slideElements.forEach((element) => {
-    observer.observe(element);
-  });
+  // slideElements.forEach((element) => {
+  //   observer.observe(element);
+  // });
 });
