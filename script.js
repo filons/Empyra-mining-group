0385b2d00cd386be5d50e94da78a9206296c37a8
Ebai99 <abibaron99@gmail.com>
2025-09-06 13:06:53 +0100

    major responsive updates

--- script.js
+++ script.js
@@ -1,82 +1,82 @@
-    const burger = document.getElementById('burger-menu');
-    const navLinks = document.getElementById('nav-links');
-    burger.addEventListener('click', () => {
-        navLinks.classList.toggle('nav-active');
-        burger.classList.toggle('toggle');
-    });
+const burger = document.getElementById("burger-menu");
+const navLinks = document.getElementById("nav-links");
+burger.addEventListener("click", () => {
+  navLinks.classList.toggle("nav-active");
+  burger.classList.toggle("toggle");
+});
 
-    window.addEventListener('resize', () => {
-        if (window.innerWidth > 768) {
-            navLinks.classList.remove('nav-active');
-            burger.classList.remove('toggle');
-        }
-    });
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 768) {
+    navLinks.classList.remove("nav-active");
+    burger.classList.remove("toggle");
+  }
+});
 
-    new Swiper('.services.swiper', {
-        loop: true,
-        spaceBetween: 30,
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-            dynamicBullets: true,
-        },
-        navigation: {
-            nextEl: '.service-nav-next',
-            prevEl: '.service-nav-prev',
-        },
-        breakpoints: {
-            0: {
-                slidesPerView: 1,
-            },
-            768: {
-                slidesPerView: 2,
-            },
-            1024: {
-                slidesPerView: 3,
-            },
-        },
-    });
+new Swiper(".services.swiper", {
+  loop: true,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".service-nav-next",
+    prevEl: ".service-nav-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
 
-    new Swiper('.projets.swiper', {
-        loop: true,
-        spaceBetween: 30,
-        pauseOnMouseEnter: true,
-        effect:'coverflow',
-        grabCursor: true,
-        centeredSlide:true,
-        coverflowEffect:{
-            rotate:50,
-            stretch:0,
-            depth:100,
-            modifier:1,
-            slideShadows:true,
-        },
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-            dynamicBullets: true,
-        },
-        navigation: {
-            nextEl: '.projet-nav-next',
-            prevEl: '.projet-nav-prev',
-        },
-        breakpoints: {
-            0: {
-                slidesPerView: 1,
-            },
-            768: {
-                slidesPerView: 2,
-            },
-            1024: {
-                slidesPerView: 3,
-            },
-        },
-    });
+new Swiper(".projets.swiper", {
+  loop: true,
+  spaceBetween: 30,
+  pauseOnMouseEnter: true,
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlide: true,
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".projet-nav-next",
+    prevEl: ".projet-nav-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
